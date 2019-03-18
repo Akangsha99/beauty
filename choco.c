bdb21c48d2fe9dbd3e6919433f66088744657c8b
Akangsha99 <48622520+Akangsha99@users.noreply.github.com>
2019-03-18 16:11:55 +0530

    Update choco.c

--- choco.c
+++ choco.c
@@ -1,1 +1,3 @@
 i was born beautiful and i know that. tell me something i don't already know.
+close your eyes and think of yourself. why? because you are the single most pretty thig in the world.
+  
